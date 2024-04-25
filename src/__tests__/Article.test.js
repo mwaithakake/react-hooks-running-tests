2d1ccd65fd23c7e048138dbbd5bd85a2fce184b6
Martha <marthanyangatiwaithaka@gmail.com>
2024-04-25 16:47:34 +0300

    new commit

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,11 +1,15 @@
+
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
 
+// import the component you wrote
 import Article from "../components/Article";
 
+// test the component
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
+  screen.debug();
 
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
